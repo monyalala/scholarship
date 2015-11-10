96292c8ea9a92caf585c751e5c8a44692f80a79d
OSP <orsereiparinha@gmail.com>
2015-11-10 09:40:35 +0700

    delete some file

--- platforms/browser/www/js/app/view/movie_view.js
+++
@@ -1,9 +0,0 @@
-var MovieView = {
-  renderList: function () {
-    var movies = MovieModel.getMovies();
-    AppTemplate.process("movie.html", {movies: movies}, function (content) {
-      $("#page-movies").html(content);
-      $("#page-movies").trigger('create');
-    });
-  }
-};
