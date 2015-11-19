7d8ee9987f92172a527e0d5a9a08871f97d9779a
khengsopheak <sopheak.kheng90001@gmail.com>
2015-11-19 11:32:51 +0700

    add MVC univerDetail

--- www/js/app/model/university_model.js
+++ www/js/app/model/university_model.js
@@ -1,7 +1,11 @@
 var UniversityModel = {
     university: [],
+    uni_fetch_id: [],
     getUniversity: function () {
         return UniversityModel.university;
+    },
+    getUniversityByID: function(){
+        return UniversityModel.uni_fetch_id;
     },
     fetch: function () {
         $.ajax({
@@ -11,6 +15,21 @@ var UniversityModel = {
             crossDomain: true,
             success: function (data) {                
                 UniversityModel.university = JSON.parse(data);                
+                UniversityView.renderList();
+            },
+            error: function (error) {
+                console.log('error ; ', error);
+            }
+        });
+    },
+    fetchById: function (id){
+        $.ajax({
+            type: "GET",
+            datatype: "JSON",
+            url: "http://scholarship.ezytech.biz/index.php/mobile/university/getList" + id,  
+            crossDomain: true,
+            success: function (data) {                
+                UniversityModel.uni_fetch_id = JSON.parse(data);                
                 UniversityView.renderList();
             },
             error: function (error) {
